1ed0a6e918bb48fff585d9b9ce276d547f9254ab
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-07-20 15:26:53 +0700

    data sharing via Xcom

--- dags/dag_with_python_operator.py
+++ dags/dag_with_python_operator.py
@@ -3,8 +3,19 @@ from airflow.operators.python import PythonOperator
 from airflow import DAG
 
 
-def greeting(name, age):
-    print(f"Hello world, my name is {name}, i am {age} years old!")
+def greeting(ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    print(f"Hello world, my name is {first_name} {last_name}, i am {age} years old!")  
+
+
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Hoang')
+    ti.xcom_push(key='last_name', value='Khang')
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=20)
 
 default_args = {
     'owner': 'khanghoang',
@@ -14,7 +25,7 @@ default_args = {
 
 with DAG(
     default_args = default_args,
-    dag_id = 'dag_using_python_operator_v2',
+    dag_id = 'dag_using_python_operator_v6',
     description = 'This is first Dag using python operator',
     start_date=datetime(2023, 7, 20),
     schedule_interval='@daily'
@@ -22,7 +33,16 @@ with DAG(
     task1 = PythonOperator(
         task_id = 'greet',
         python_callable=greeting,
-        op_kwargs={'name': 'Khang', 'age': 20}
+        #op_kwargs={'age': 20}
+    )
+    task2 = PythonOperator(
+        task_id = 'get_name',
+        python_callable=get_name
+    )
+    task3 = PythonOperator(
+        task_id = 'get_age',
+        python_callable=get_age
     )
 
-    task1
+    #task1
+    [task2,task3] >> task1
